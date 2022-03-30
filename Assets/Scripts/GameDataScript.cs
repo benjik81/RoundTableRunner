54f230f033fea86513a7f233e0a80a674538dc14
Benjik <ben12pr@hotmail.com>
2022-03-30 18:34:23 +0200

    {Update} GameDataScript
    Adding comments

--- Assets/Scripts/GameDataScript.cs
+++ Assets/Scripts/GameDataScript.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 
+//allows to add a Unity option to create the GameData object
 [CreateAssetMenu(menuName = "Create GameData")]
 
 public class GameDataScript : ScriptableObject
 {
+    //The data saved here is the player's name and score.
     public string PlayerName;
 
     public int Score = 0;
