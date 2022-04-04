5ff9859bb8e9ccaf42a5aa7c07a55eb9066b93ca
VixyLeDieu <tvv3show@gmail.com>
2022-04-04 15:44:19 +0200

    {Add} Scripts for Fire Ball and Lady of the Lake
    Nothing in there yet

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : Bonus
+{
+    public override void Effect()
+    {
+        base.Effect();
+    }
+}
---
+++ Assets/Scripts/LOTL.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LOTL : Bonus
+{
+    public override void Effect()
+    {
+        base.Effect();
+    }
+}
