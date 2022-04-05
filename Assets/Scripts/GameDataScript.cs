bfee670ea3ec363de1bb67f58443139b85936e74
Benjik <ben12pr@hotmail.com>
2022-04-05 16:29:05 +0200

    {Update} GameDataScript
    Added the volume value of the game.

--- Assets/Scripts/GameDataScript.cs
+++ Assets/Scripts/GameDataScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class GameDataScript : ScriptableObject
 {
-    //The data saved here is the player's name and score.
+    //The data saved here is the player's name and score and key codes and the volume's value.
     public string playerName;
 
     public int score = 0;
@@ -14,6 +14,8 @@ public class GameDataScript : ScriptableObject
     public KeyCode percevalKeyCode = KeyCode.J;
     public KeyCode lancelotKeyCode = KeyCode.G;
     public KeyCode gauvainKeyCode = KeyCode.F;
+
+    public float volume = 1.0f;
 
     private void OnEnable()
     {
