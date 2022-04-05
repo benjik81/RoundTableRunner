177a4e0fddfd31031881847a8a936f10524f1d52
VixyLeDieu <tvv3show@gmail.com>
2022-04-05 22:57:58 +0200

    {Add} Sword script
    Now spawn sword in repetition that destroy any obstacle it touches

---
+++ Assets/Scripts/Bonus/Sword.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : Bonus
+{
+    public SwordData swordData;
+    Vector3 shootingPos;
+    PlayerScript knight;
+    float maxTimer;
+    float maxTimertemp;
+    bool started;
+    public override void Effect(PlayerScript player)
+    {
+        started = true;
+        knight = player;
+        maxTimer = swordData.duration;
+        maxTimertemp = maxTimer;
+
+        foreach (Transform child in transform) // Removes all collider and graphics
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+    public override void Update()
+    {
+        if (!started)
+        {
+            transform.Translate(new Vector3(0, 0, 1) * -obstacleData.speed * GameManager.instance.scrollingMultiplier * Time.deltaTime);
+        }
+        else
+        {
+            if (maxTimer <= maxTimertemp - swordData.delay)
+            {
+                maxTimertemp = maxTimer;
+                Shoot();
+            }
+
+
+            if (maxTimer < 0)
+            {
+                knight.RemoveBonus();
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                maxTimer -= Time.deltaTime;
+            }
+        }
+        
+    }
+
+    void Shoot()
+    {
+        shootingPos = knight.gameObject.transform.position + swordData.offSet;
+        GameObject tempSword = Instantiate(swordData.swordPrefab);
+        tempSword.transform.position = shootingPos;
+        Projectile tempProj = tempSword.GetComponent<Projectile>();
+
+        tempProj.SetProjectile(swordData.projSpeed, swordData.armor);
+    }
+}
