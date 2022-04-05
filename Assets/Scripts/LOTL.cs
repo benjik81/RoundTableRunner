81834f6cd1e0564514124376f16fa4c264c6e050
VixyLeDieu <tvv3show@gmail.com>
2022-04-05 19:36:00 +0200

    {Update} TestScene and LOTL script
    LOTL buff now correctly gives invincibility to all knights. Updated test scene for better testing

--- Assets/Scripts/LOTL.cs
+++ Assets/Scripts/LOTL.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class LOTL : Bonus
 {
+    public LOTLData lOTLData;
+    float tempValue;
+
     public override void Effect()
     {
-        base.Effect();
+        tempValue = GameManager.instance.scrollingMultiplier;
+
+        foreach (var item in GameManager.instance.knights)
+        {
+            item.GiveInvincibility(lOTLData.duration);
+        }
+
+        GameManager.instance.scrollingMultiplier = lOTLData.accelerationSpeed;
+
+        StartCoroutine(Countdown());
+    }
+
+    
+    private IEnumerator Countdown()
+    {
+        while (GameManager.instance.knights[0].isInvincible)
+        {
+            yield return null;
+        }
+
+        GameManager.instance.scrollingMultiplier = tempValue;
+
     }
 }
