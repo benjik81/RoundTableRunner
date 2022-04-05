f8d51fb24a0d1b591ca9c4eaa360410e2770fd6a
VixyLeDieu <tvv3show@gmail.com>
2022-04-05 19:33:44 +0200

    {Add} LOTL Datas
    Added a scriptable object for the lotl buff and created the datas that go with it

---
+++ Assets/Scripts/Data/LOTLData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LOTL", menuName = "ScriptableObject/BonusData/LadyOfTheLake")]
+public class LOTLData : ScriptableObject
+{
+    public float duration;
+    public float accelerationSpeed;
+    public Sprite auraSprite;
+}
