e64266e3d1353556efbbee569c426f445d9fd057
VixyLeDieu <tvv3show@gmail.com>
2022-04-05 20:21:37 +0200

    {Add} Data scriptable for all bonuses
    Only fireball and lotl are done

---
+++ Assets/Scripts/Data/FireBallData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FireBall", menuName = "ScriptableObject/BonusData/Fire Ball")]
+public class FireBallData : ScriptableObject
+{
+    public GameObject fireBallPrefab;
+    public float projSpeed;
+    public int armor; // how many hits can they take
+    public Vector3 offSet; // where does the fireball start
+}
