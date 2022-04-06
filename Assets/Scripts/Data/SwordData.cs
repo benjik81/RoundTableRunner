732949ae94c8da8e58c213feef6caeef76f1c099
VixyLeDieu <tvv3show@gmail.com>
2022-04-06 21:30:08 +0200

    {Update} Bonus datas
    All bonuses now derivates from bonus data scriptable object

--- Assets/Scripts/Data/SwordData.cs
+++ Assets/Scripts/Data/SwordData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Sword", menuName = "ScriptableObject/BonusData/Sword")]
-public class SwordData : ScriptableObject
+public class SwordData : BonusData
 {
     public float duration;
     public Vector3 offSet;
@@ -12,4 +12,5 @@ public class SwordData : ScriptableObject
 
     public float delay;
     public GameObject swordPrefab;
+
 }
