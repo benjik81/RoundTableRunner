4923b957e7cc1063b6d28fe027f4625b74f21a6a
Benjik <ben12pr@hotmail.com>
2022-04-07 16:25:53 +0200

    {Update} GameOverScript
    Added the highscore in save & gameData.

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -8,10 +8,14 @@ public class GameOverScript : MonoBehaviour
 {
     [SerializeField] private TMP_Text score_Text;
     [SerializeField] private GameDataScript gameData;
+    private int score;
 
     public void Start()
     {
+        score = gameData.score;
         DisplayScore();
+        UpdateHighScore();
+        SaveSystem.SaveData(gameData);
     }
     public void LoadMenu()
     {
@@ -25,7 +29,7 @@ public class GameOverScript : MonoBehaviour
 
     public void DisplayScore()
     {
-        score_Text.text = gameData.score.ToString();
+        score_Text.text = score.ToString();
     }
 
     public void TryAgain()
@@ -34,5 +38,11 @@ public class GameOverScript : MonoBehaviour
             gameData.highscore = gameData.score;
         gameData.score = 0;
         SceneManager.LoadScene("JumpTestScene");
+    }
+
+    public void UpdateHighScore()
+    {
+        if (gameData.score > gameData.highscore)
+            gameData.highscore = gameData.score;
     }
 }
