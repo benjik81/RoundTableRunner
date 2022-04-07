237c916989b9c7b7f2c1d6b90f498f27243bb772
eclair81 <bastienpic.bp@gmail.com>
2022-04-07 10:02:09 +0200

    {Fix} ScoreScript
    Score is now incremented when the obstacle is going out of the trigger (not in)

--- Assets/Scripts/Score/ScoreScript.cs
+++ Assets/Scripts/Score/ScoreScript.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class ScoreScript : MonoBehaviour
 {
     [SerializeField] private GameDataScript gameData;
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Obstacle" || other.tag == "Projectile")
         {
             gameData.score += 100;
+            Debug.Log("current score: " + gameData.score);
         }
     }
 }
